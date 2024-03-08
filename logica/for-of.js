9dfe1fb4a04785a5f8e3162842c43222be3b0a7d
evanvidel <evanvidel@gmail.com>
2024-03-08 11:10:56 -0300

    repeat loops

--- logica/for-of.js
+++ logica/for-of.js
@@ -1,22 +1,41 @@
 //const nome = 'Luiz Otávio';
-const nome = ['Luiz' ,'Otávio', 'Henrique'];
+const nomes = ["Luiz", "Otávio", "Henrique"];
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+}
+
+/**
+ * for classico - Geralnmente com interáveis (array ou strings)
+ * for in - Retorna inde ou chave (string, array ou objatos)
+ * for of - Retorna o valor em si (interáveis, arrays ou strings)
+ */
+
+/* for(let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+} */
+
 
 //padrão
-console.log('>>>>> for Padrão <<<<<<');
-for(let i = 0; i < nome.length; i++) {
-    console.log(nome[i]);
+console.log(">>>>> for Padrão <<<<<<");
+for (let i = 0; i < nomes.length; i++) {
+  console.log(nomes[i]);
 }
- //For in
- console.log('>>>>> for in <<<<<<');
- for(let i  in nome) {
-    console.log(nome[i]);
- }
+//For in
+console.log(">>>>> for in <<<<<<");
+for (let i in nomes) {
+  console.log(nomes[i]);
+}
 
- console.log('>>>>> for of <<<<<<');
- 
- //for of 
- for(let valor of nome){
-     console.log(valor);
-    }
-    
-console.log('>>>>> forEach <<<<<<');
+console.log(">>>>> for of <<<<<<");
+
+//for of
+for (let valor of nomes) {
+  console.log(valor);
+}
+
+console.log(">>>>> forEach <<<<<<");
+nomes.forEach(function (valor, indice, array) {
+  console.log(valor, indice, array);
+});
