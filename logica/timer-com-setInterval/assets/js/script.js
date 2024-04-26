bd262fb92620fab8f0c8145141f544d806b46ca4
evanvidel <evanvidel@vgmail.com>
2024-04-26 09:27:30 -0300

    simple to-do list with localStorage

--- logica/timer-com-setInterval/assets/js/script.js
+++ logica/timer-com-setInterval/assets/js/script.js
@@ -1,34 +1,41 @@
-function criaHoraDosSegudos(segundos) {
-  const data = new Date(segundos * 1000);
-  return data.toLocaleTimeString('pt-BR', {
-    hour12: false,
-    timeZone: 'UTC',
+function relogio() {
+  function criaHoraDosSegudos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'UTC',
+    });
+  }
+
+  const relogio = document.querySelector('.relogio');
+  let segundos = 0;
+  let timer;
+
+  function iniciarRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegudos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('zerar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      segundos = 0;
+    }
+    if (el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciarRelogio();
+    }
+    if (el.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
   });
 }
-
-console.log(criaHoraDosSegudos(10));
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let segundos = 0;
-
-function iniciarRelogio() {
-  const timer = setInterval(function () {
-    segundos++;
-    relogio.innerHTML = criaHoraDosSegudos(segundos);
-  }, 1000);
-}
-
-iniciar.addEventListener('click', function (e) {
-  iniciarRelogio();
-});
-
-pausar.addEventListener('click', function (e) {
-  relogio.innerHTML = 'cliquei no pausar';
-});
-
-zerar.addEventListener('click', function (e) {
-  relogio.innerHTML = 'cliquei no zerar';
-});
+relogio();
